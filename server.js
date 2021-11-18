e41c516fa292727289d39d88c24d0d62b2903b2f
Alex Watkins <38054672+afw821@users.noreply.github.com>
2021-11-17 21:55:14 -0500

    Moved order of middleware for deployment issue

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const app = express();
 //env variables
 require("dotenv").config();
 //startup
-require("./startup/middleware")(app);
 require("./startup/routes")(app);
+require("./startup/middleware")(app);
 require("./startup/db")(app);
